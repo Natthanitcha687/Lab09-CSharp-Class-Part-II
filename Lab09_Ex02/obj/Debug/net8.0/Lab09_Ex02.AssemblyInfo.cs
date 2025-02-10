72969717050f0c3a3b7c3116e6ed67de5abf3dc9
natthanitcha687 <babynitcha0@gmail.com>
2025-02-10 21:50:33 +0700

    update04

--- Lab09_Ex02/obj/Debug/net8.0/Lab09_Ex02.AssemblyInfo.cs
+++ Lab09_Ex02/obj/Debug/net8.0/Lab09_Ex02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09_Ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e6a2aadbdebf2b8c5e065f4ea21e9ff08d0653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01ca655446c19dbe78d2c0c72b1762a3b045e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09_Ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09_Ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
